ff999560954effa96cfc3a38a097da092fb08183
Mohd Asad Hussain <asadhussain9711@gmail.com>
2016-02-17 19:20:26 +0530

    Update Net.java
    If the file already exists then it gets overwritten.

--- Net.java
+++ Net.java
@@ -4,8 +4,13 @@ class Net
 {
 	public static void main(String s[])throws IOException,MalformedURLException
 	{
-		URL url=new URL("http://www.google.com");
-		FileWriter fw=new FileWriter("F:\\fread.txt");//path of the file where to save the data
+		// URL url=new URL("http://www.facebook.com/");
+		// URL url=new URL("http://www.google.com/");
+		URL url=new URL("http://jmi.ac.in/");
+		// URL url=new URL("http://www.quora.com/");
+
+
+		FileWriter fw=new FileWriter("F:\\frea.txt",false);
 		PrintWriter pw=new PrintWriter(fw);
 		BufferedReader bf=new BufferedReader(new InputStreamReader(url.openStream()));
         String str="";
